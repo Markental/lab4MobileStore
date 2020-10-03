75c9a1fa08ad13ceb230c159babcd29ccd4b8b43
Olzhas Karatayev <karataevolzhas@gmail.com>
2020-10-03 09:13:46 +0600

    Добавьте файлы проекта.

---
+++ lab4MobileStore/Data/MobileContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab4MobileStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab4MobileStore.Data
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public MobileContext(DbContextOptions<MobileContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=mobilestoredb.db");
+        }
+    }
+}
---
+++ lab4MobileStore/Data/MobileContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace lab4MobileStore.Data
+{
+    public class MobileContextFactory : IDesignTimeDbContextFactory<MobileContext>
+    {
+        public MobileContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MobileContext>();
+            optionsBuilder.UseSqlite("Data Source=mobilestoredb.db");
+
+            return new MobileContext(optionsBuilder.Options);
+        }
+    }
+}
